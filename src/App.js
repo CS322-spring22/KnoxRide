6115b4b877d9dca5c1ec70da3540d32feb56cd5c
Kieu Linh Vuong <kieulinh.jan22@gmail.com>
2022-04-17 15:07:29 -0500

    link routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from "./Header";
-import Feed from "./Feed";
+import RoutesController from './RoutesController';
+//import HomePage from "./HomePage";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-
-      <div className="app_body">
-        <Feed />
-      </div>
+       <div className="app_body">
+         <RoutesController />
+       </div>
 
     </div>
   );
