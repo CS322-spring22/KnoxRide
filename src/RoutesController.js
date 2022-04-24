018ec5536d17eeccf27d1060c5fd481e6fe196fa
Kieu Linh Vuong <kieulinh.jan22@gmail.com>
2022-04-24 15:14:33 -0500

    set up for driver schedule page

--- src/RoutesController.js
+++ src/RoutesController.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Route, Routes} from 'react-router-dom';
 import DriverFeed from './DriverNewsFeed/DriverFeed';
+import DriverProfilePage from './DriverProfile/DriverProfilePage';
+import DriverSchedulePage from './DriverSchedule/DriverSchedulePage';
 import HomePage from './HomePage';
 import Applog from './Applog';
 
@@ -9,7 +11,8 @@ function RoutesController() {
         
             <Routes>
                 <Route exact path="/" element={<DriverFeed />} />
-                <Route exact path="/driverprofile" element={<DriverProfile />} />
+                <Route exact path="/driverprofile" element={<DriverProfilePage />} />
+                <Route exact path="/driverschedule" element={<DriverSchedulePage />} />
                 <Route exact path="/login" element={<Applog />} />
                 <Route exact path="/homepage" element={<HomePage />} />
             </Routes>
