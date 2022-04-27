3bdfdc0166c1243fae8d51ead21d59037f77b791
Kieu Linh Vuong <kieulinh.jan22@gmail.com>
2022-04-27 01:34:06 -0500

    add firebase file

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBHP9uPmi9IKjbxn4eyjLhmiNySvJx6ExQ",
+    authDomain: "knoxride-929d9.firebaseapp.com",
+    projectId: "knoxride-929d9",
+    storageBucket: "knoxride-929d9.appspot.com",
+    messagingSenderId: "350221082279",
+    appId: "1:350221082279:web:8badc262232335b5560582",
+    measurementId: "G-78MB682WVF"
+  };
