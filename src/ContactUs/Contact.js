27a827a67024dc6c0afb81bb65e8f67989966866
k0j1314 <k0j1314@LAPTOP-J9RAIGSV.localdomain>
2022-04-27 17:11:18 -0500

    files got reverted whern merging, fixed now

--- src/ContactUs/Contact.js
+++ src/ContactUs/Contact.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import "./Contact.css";
 import logo from "../UserHomePage/logopic.png";
 import "../RequestPage/Request.css";
@@ -91,83 +89,4 @@ function contactUs() {
   );
 }
 
-=======
-=======
->>>>>>> fe3fa22b3b9c85e656e7b280429b3e8be108e2db
-import './Contact.css';
-import logo from '../UserHomePage/logopic.png';
-
-function contactUs() {
-  return (
-    <div className="AppUser">
-      <header className="headerUser">
-        <nav className="navbarUser">
-            <nav className = 'logopic' >
-            <a href="/homepage">
-            <img src={logo} alt="logo" />
-            </a>
-            </nav>
-            <ul className="nav-menu">
-                <li className="nav-item">
-                    <a href="/request" className="nav-link">Make a Request</a>
-                </li>
-                <li className="nav-item">
-                    <a href="#" className="nav-link">History</a>
-                </li>
-                <li className="nav-item">
-                    <a href="/" className="nav-link">Become a Driver</a>
-                </li>
-                <li className="nav-item">
-                    <a href="/contact" className="nav-link">Contact Us</a>
-                </li>
-                <li className = "nav-item">
-                    <a href = '#' className = 'nav-link '>My Account</a>
-                </li>
-                <li className="nav-item">
-                    <a href="#" className="nav-link">Sign Out</a>
-                </li>
-            </ul>
-
-            </nav>
-</header>
-
-<form action="//submit.form" id="ContactUs100" method="post" onsubmit="return ValidateForm(this);">
-          <table style={{width: '100%', maxWidth: '1500px', border: 0}} cellSpacing={0} cellPadding={8}>
-            <tbody><tr> <td>
-                  <label htmlFor="Name">Name*:</label>
-                </td> <td>
-                  <input name="Name" type="text" maxLength={60} style={{width: '100%', maxWidth: '100px'}} />
-                </td> </tr> <tr> <td>
-                  <label htmlFor="PhoneNumber">Phone number:</label>
-                </td> <td>
-                  <input name="PhoneNumber" type="text" maxLength={43} style={{width: '100%', maxWidth: '250px'}} />
-                </td> </tr> <tr> <td>
-                  <label htmlFor="FromEmailAddress">Email address*:</label>
-                </td> <td>
-                  <input name="FromEmailAddress" type="text" maxLength={90} style={{width: '100%', maxWidth: '250px'}} />
-                </td> </tr> <tr> <td>
-                  <label htmlFor="Comments">Comments*:</label>
-                </td> <td>
-                  <textarea name="Comments" rows={7} cols={40} style={{width: '100%', maxWidth: '350px'}} defaultValue={""} />
-                </td> </tr> <tr> <td>
-                  * - required fields
-                </td> <td>
-                  <div style={{float: 'right'}}><a href="#" id="lnk100" title="form to email">form to email</a></div>
-                  <input name="skip_Submit" type="submit" defaultValue="Submit" />
-                </td> </tr>
-            </tbody></table>
-        </form>
-</div>
-
-
-
-
-);
-}
-
-
-<<<<<<< HEAD
->>>>>>> fe3fa22b3b9c85e656e7b280429b3e8be108e2db
-=======
->>>>>>> fe3fa22b3b9c85e656e7b280429b3e8be108e2db
 export default contactUs;
