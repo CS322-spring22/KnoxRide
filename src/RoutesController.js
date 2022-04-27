269932ec3fa89faade49c59e6aa24a9da9f1558a
k0j1314 <k0j1314@LAPTOP-J9RAIGSV.localdomain>
2022-04-27 17:01:56 -0500

    request page is done, looking to merge in contact us page

--- src/RoutesController.js
+++ src/RoutesController.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import {Route, Routes} from 'react-router-dom';
+<<<<<<< HEAD
 import Feed from './Feed';
 import HomePage from './UserHomePage/userHomePage';
+=======
+import HomePage from './UserHomePage/userHomePage';
+import DriverFeed from './DriverNewsFeed/DriverFeed';
+import DriverSchedulePage from './DriverSchedule/DriverSchedulePage';
+>>>>>>> fe3fa22b3b9c85e656e7b280429b3e8be108e2db
 import Applog from './Applog';
 import Request from './RequestPage/Request.js';
 import Contact from './ContactUs/Contact.js';
@@ -10,7 +16,8 @@ function RoutesController() {
     return (
         
             <Routes>
-                <Route exact path="/" element={<Feed />} />
+                <Route exact path="/" element={<DriverFeed />} />
+                <Route exact path="/driverschedule" element={<DriverSchedulePage />} />
                 <Route exact path="/login" element={<Applog />} />
                 <Route exact path="/homepage" element={<HomePage />} />
                 <Route exact path = "/request" element = {<Request/>} />
--- src/UserHomePage/userHomePage.js
+++ src/UserHomePage/userHomePage.js
@@ -5,34 +5,39 @@ function HomePage() {
     <div className="AppUser">
       <header className="headerUser">
         <nav className="navbarUser">
-          <nav className="logopicuserhome">
+          <nav className="logopic">
             <a href="/homepage">
-              <img className="userHome" src={logo} alt="logo" />
+              <img src={logo} alt="logo" />
             </a>
           </nav>
-          <ul className="nav-menu-home">
-            <li className="nav-item-home">
-              <a href="/request" className="nav-link-home">
+          <ul className="nav-menu">
+            <li className="nav-item">
+              <a href="/request" className="nav-link">
                 Make a Request
               </a>
             </li>
-            <li className="nav-item-home">
-              <a href="#" className="nav-link-home">
+            <li className="nav-item">
+              <a href="#" className="nav-link">
                 History
               </a>
             </li>
-            <li className="nav-item-home">
-              <a href="/" className="nav-link-home">
+            <li className="nav-item">
+              <a href="/" className="nav-link">
                 Become a Driver
               </a>
             </li>
-            <li className="nav-item-home">
-              <a href="/contact" className="nav-link-home">
+            <li className="nav-item">
+              <a href="/contact" className="nav-link">
                 Contact Us
               </a>
             </li>
-            <li className="nav-item-home">
-              <a href="#" className="nav-link-home">
+            <li className="nav-item">
+              <a href="#" className="nav-link">
+                My Account
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-link">
                 Sign Out
               </a>
             </li>
@@ -41,14 +46,14 @@ function HomePage() {
       </header>
       <div className="rowHome">
         <div className="columnHome">
-          <h1 className="homeColumn">MY REQUESTS</h1>
+          <h1>MY REQUESTS</h1>
         </div>
 
         <div className="columnHome">
-          <h1 className="homeColumn">PENDING</h1>
+          <h1>PENDING</h1>
         </div>
         <div className="columnHome">
-          <h1 className="homeColumn">SOMETHING HERE</h1>
+          <h1>SOMETHING HERE</h1>
         </div>
       </div>
     </div>
