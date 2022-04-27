acebc659f76f6bd247ab4c979a1c821207d6e3b1
k0j1314 <k0j1314@LAPTOP-J9RAIGSV.localdomain>
2022-04-26 20:06:27 -0500

    <input type="text" name="name" placeholder="Street address" />           <input type="text" name="name" placeholder="Street address line 2" />           <div className="city-item">             <input type="text" name="name" placeholder="City" />             <select>               <option value>Country</option>               <option value={1}>Russia</option>               <option value={2}>Germany</option>               <option value={3}>France</option>               <option value={4}>Armenia</option>               <option value={5}>USA</option>             </select>           </div> fixing name titles for request page, issues with the form

--- src/UserHomePage/userHomePage.js
+++ src/UserHomePage/userHomePage.js
@@ -5,43 +5,43 @@ function HomePage() {
     <div className="AppUser">
       <header className="headerUser">
         <nav className="navbarUser">
-            <nav className = 'logopic' >
+            <nav className = 'logopicuserhome' >
             <a href="/homepage">
-            <img src={logo} alt="logo" />
+            <img className = 'userHome' src={logo} alt="logo" />
             </a>
             </nav>
-            <ul className="nav-menu">
-                <li className="nav-item">
-                    <a href="/request" className="nav-link">Make a Request</a>
+            <ul className="nav-menu-home">
+                <li className="nav-item-home">
+                    <a href="/request" className="nav-link-home">Make a Request</a>
                 </li>
-                <li className="nav-item">
-                    <a href="#" className="nav-link">History</a>
+                <li className="nav-item-home">
+                    <a href="#" className="nav-link-home">History</a>
                 </li>
-                <li className="nav-item">
-                    <a href="/" className="nav-link">Become a Driver</a>
+                <li className="nav-item-home">
+                    <a href="/" className="nav-link-home">Become a Driver</a>
                 </li>
-                <li className="nav-item">
-                    <a href="/contact" className="nav-link">Contact Us</a>
+                <li className="nav-item-home">
+                    <a href="/contact" className="nav-link-home">Contact Us</a>
                 </li>
-                <li className = "nav-item">
-                    <a href = '#' className = 'nav-link'>My Account</a>
+                <li className = "nav-item-home">
+                    <a href = '#' className = 'nav-link-home'>My Account</a>
                 </li>
-                <li className="nav-item">
-                    <a href="#" className="nav-link">Sign Out</a>
+                <li className="nav-item-home">
+                    <a href="#" className="nav-link-home">Sign Out</a>
                 </li>
             </ul>
         </nav>
 </header>
 <div className="rowHome">
   <div className="columnHome">
-  <h1>MY REQUESTS</h1>
+  <h1 className = 'homeColumn'>MY REQUESTS</h1>
   </div>
   
   <div className="columnHome">
-  <h1>PENDING</h1>
+  <h1 className = 'homeColumn'>PENDING</h1>
   </div>
   <div className="columnHome">
-  <h1>SOMETHING HERE</h1>
+  <h1 className = 'homeColumn'>SOMETHING HERE</h1>
   </div>
 </div> 
 
